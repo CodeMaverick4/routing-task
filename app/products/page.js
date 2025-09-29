8d1390defd9c527d89fa58519c2ee24e3fdc7751
Burhan Ahmed <burhan.ahmed@advantal.net>
2025-09-29 14:58:20 +0530

    routing task.

---
+++ app/products/page.js
@@ -0,0 +1,8 @@
+export default function Product() {
+    return (
+        <main>
+            <h1>Products Page</h1>
+            <p>This is a simple placeholder for the products page.</p>
+        </main>
+    );
+}
